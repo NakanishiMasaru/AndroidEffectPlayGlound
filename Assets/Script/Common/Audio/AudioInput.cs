4f3f8f1cb2430ddcb3218a14275b34e214c6d322
NakanishiMasaru <skckmdtk2@gmail.com>
2020-07-20 04:21:36 +0900

    スペクトラム荒作成

---
+++ Assets/Script/Common/Audio/AudioInput.cs
@@ -0,0 +1,28 @@
+﻿using Script.Texture.Texture;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Script.Common.Audio
+{
+    public class AudioInput : MonoBehaviour
+    {
+        [SerializeField] private AudioSource audioSource;
+        [SerializeField] private float[] spectrum = new float[256];
+        [SerializeField] private ModifyTexture mod;
+        void Start()
+        {
+            audioSource = GetComponent<AudioSource>();
+            audioSource.clip = Microphone.Start(null, true, 10, 44100);
+            audioSource.loop = true;
+            //while (Microphone.GetPosition(null) < 0) { }
+            audioSource.Play();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
+            mod.spectrum = spectrum;
+        }
+    }
+}
---
+++ Assets/Script/Texture/Texture/ModifyTexture.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Script.Texture.Texture
+{
+    public class ModifyTexture : MonoBehaviour
+    {
+        [SerializeField] private Texture2D m_texture;
+        private Color[] _colors;
+        public float[] spectrum;
+        private Renderer rend;
+        [Range(0,500000)]
+        [SerializeField] private float gain = 1f;
+        
+        // Start is called before the first frame update
+        void Start()
+        {    
+                rend = GetComponent<Renderer>();
+                m_texture = Instantiate((Texture2D) rend.material.mainTexture);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            SetTex();
+        }
+
+        void SetTex()
+        {
+            _colors = m_texture.GetPixels();
+            var len = m_texture.texelSize.x;
+            m_texture.filterMode = FilterMode.Point;
+            for (int i = 0; i < spectrum.Length; i++)
+            {
+                _colors.SetValue(new Color(spectrum[i]*gain,spectrum[i]*gain,spectrum[i]*gain), i);
+            }
+            m_texture.SetPixels(_colors);
+            m_texture.Apply();
+            rend.material.mainTexture = m_texture;
+        }
+    }
+}
